e91582c1b0ee99d57e7ba5fb5dabc3e8eda20b95
Beth Yates <blyates@users.noreply.github.com>
2020-02-14 15:05:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,10 @@ yum -y install gcc
 yum -y install zlib-devel
 yum -y install sudo
 yum -y install git
+
+service iptables stop;
+chkconfig iptables off;
+rm -rf /etc/rc.d/init.d/iptables
+
+yum -y install perl-CPAN
+curl -L http://cpanmin.us | perl - App::cpanminus
